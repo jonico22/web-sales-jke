69c28d56a07ce2a324794a062175e4f4aa40d7bd
Jose Luis Yana <joseyananicolas@gmail.com>
2026-02-14 09:52:07 -0500

    Configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import node from '@astrojs/node';
-
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  },
-  adapter: cloudflare(),
-  server: {
-    host: true, // Crucial para Docker/Coolify
-    port: 4321  // O el puerto que prefieras
   }
 });
