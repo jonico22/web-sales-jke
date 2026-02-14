8e24c50ee5af45765f2a2fa0aa3a69f0ffe04af3
Jose Luis Yana <joseyananicolas@gmail.com>
2026-02-13 22:33:07 -0500

    Configuración SSR para Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare(),
   server: {
     host: true, // Crucial para Docker/Coolify
     port: 4321  // O el puerto que prefieras
